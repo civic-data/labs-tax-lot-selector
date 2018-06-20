9b19d746c49d50b16e01a88c74e16e961abcc70e
ralph yozzo <ralph@brooklynmarathon.com>
2018-06-20 08:56:52 -0400

    Add example data for article 7 open petitions

--- getbbls.sh
+++ getbbls.sh
@@ -1,4 +1,4 @@
 #!
-# fails
-cut -c1-16 ~/Downloads/bklyn.txt  |sed "s/ //g" |grep "^3" |sort|uniq -c |sort -rn |cut -c 9- |head -10000 |paste -s -d,
+cut -c1-16 bklyn.txt  |sed "s/ //g" |grep "^3" |sort|uniq -c |sort -rn |cut -c 9- |head -20000 |paste -s -d,
+# cut -c1-16 ~/Downloads/bklyn.txt  |sed "s/ //g" |grep "^3" |sort|uniq -c |sort -rn |cut -c 9- |head -10000 |paste -s -d,
 # cut -c1-16 ~/Downloads/bklyn.txt  |sed "s/ //g" |grep "^3" |sort|uniq -c |sort -rn |cut -c 9- |head -600 |paste -s -d,
